6a6a9dc1fbcd3a4f99d36d0b1bdcb4d0378dbd8d
SATHISH <50115705+SathishPaloju@users.noreply.github.com>
2021-03-04 22:34:15 +0530

    update_1

--- app.py
+++ app.py
@@ -1,46 +1,45 @@
+
+# fit scaler on training data
+from sklearn.preprocessing import MinMaxScaler
 import flask
 import pickle
 import pandas as pd
-import numpy as np
-import imblearn
 import werkzeug
+import numpy as np
 from flask import Flask, render_template, url_for, request
-import pickle
-from bokeh.models import ColumnDataSource
-from bokeh.plotting import figure
-from bokeh.transform import factor_cmap
-from bokeh.embed import components
-from sklearn.preprocessing import MinMaxScaler
-#load models at top of app to load into memory only one time
 
-with open(r'models/model_eclf_new.pkl', 'rb') as f:
+
+#load models at top of app to load into memory only one time
+with open(r'models/model_eclf_new_.pkl', 'rb') as f:
     ensemble_model_new = pickle.load(f)
 
-from sklearn.preprocessing import MinMaxScaler
-# fit scaler on training data
+#load models at top of app to load into memory only one time
 
+#Loading the dataset onto running environment 
 df_train = pd.read_csv(r'data/data.csv')
 
-
+#app is an object of home page using Flask package
 app = flask.Flask(__name__,template_folder='templates')
 
-
+#route for home page
 @app.route('/')
 def main():
     return (flask.render_template('index.html'))
 
+#route for EDA_show page
 @app.route('/EDA_show')
 def report():
     return (flask.render_template('EDA_show.html'))
 
+#route for predict page
 @app.route("/predict", methods=['GET', 'POST'])
 def predict():
     if flask.request.method == 'GET':
         return (flask.render_template('predict.html'))
     if flask.request.method == 'POST':
         col = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed',
-               'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount',
-               'Loan_Amount_Term', 'Credit_History', 'Property_Area']
+        'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount',
+        'Loan_Amount_Term', 'Credit_History', 'Property_Area']
         Dependents = float(flask.request.form['Dependents'])
         #print(Dependents)
         Gender = flask.request.form['Gender']
@@ -62,34 +61,38 @@ def predict():
         Credit_History = flask.request.form['Credit_History']
         LoanAmount = flask.request.form['LoanAmount']
         #print(LoanAmount)
-        
+        #Data preprocessing
         if Property_Area == "Semiurban":
             Property_Area = 2
         elif Property_Area == "Urban":
             Property_Area = 1
         else:
             Property_Area = 0
+        
         if Gender == "Female":
             Gender = 0
         else:
             Gender =1
+
         if Married == "Yes":
-            Married = 0
-        else:
             Married = 1
+        else:
+            Married = 0
+
         if Education == "Graduate":
             Education = 1
         else:
             Education =0
+
         if Self_Employed == "Yes":
             Self_Employed = 1
         else:
             Self_Employed = 0
         
 
-        #create deep copy
+        #create an empty dataframe using pandas
         temp = pd.DataFrame(index=[1])
-        
+        #assign value into the dataframe
         temp["Gender"] = Gender
         temp["Married"] = Married
         temp["Dependents"] = Dependents
@@ -101,10 +104,10 @@ def predict():
         temp["Loan_Amount_Term"] = Loan_Amount_Term
         temp["Credit_History"] = Credit_History
         temp["Property_Area"] = Property_Area
-        #print(temp)
-        #print(df_train)
-        #print(temp.columns)
-        #print(df_train.columns)
+        print(temp)
+        print(df_train)
+        print(temp.columns)
+        print(df_train.columns)
         # data normalization with sklearn
         # fit scaler on training data
         norm = MinMaxScaler()
@@ -114,6 +117,7 @@ def predict():
         #make prediction
         scale = pd.DataFrame(scale, columns=temp.columns)
         pred = ensemble_model_new.predict(scale)
+        print(pred)
         """Index(['Gender', 'Married', 'Dependents', 'Education', 'ApplicantIncome',
             'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History',
             'Property_Area'],
@@ -125,7 +129,7 @@ def predict():
         if pred ==0:
             res = 'Loan Denied!'
         else:
-            res = 'Congratulations! Approved!'
+            res = 'Congratulations! Loan Approved!'
         #render form again and add prediction)'''
         return flask.render_template(r'predict.html', result=res)
 if __name__ == '__main__':
