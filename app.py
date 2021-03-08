9ff87b7c687bec75c994d3247ef115d28cb8d7a8
SATHISH <50115705+SathishPaloju@users.noreply.github.com>
2021-03-09 01:54:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ def predict():
         #render form again and add prediction)'''
         return flask.render_template(r'predict.html', result=res)
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(debug=True)
